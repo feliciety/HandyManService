6639c57d1d8dd97ac2b55f2267add291f17ef613
Fe Anne L. Malasartw <159458354+feliciety@users.noreply.github.com>
2024-11-18 19:34:17 +0800

    SignupPage ongoing

--- src/main/java/project/demo/SignupPageController.java
+++ src/main/java/project/demo/SignupPageController.java
@@ -3,7 +3,7 @@ package project.demo;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class SignupPageController {
     @FXML
     private Label welcomeText;
 
